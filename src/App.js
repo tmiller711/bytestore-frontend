eb6b207f117de8604415977b96191a2f9f143111
Tristin <tmille2004@gmail.com>
2023-02-04 21:55:42 -0600

    creates blank auth pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, {useEffect, useState} from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import HomePage from "./pages/homepage/HomePage";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import PasswordReset from "./pages/auth/PasswordReset";
 
 function App() {
   return (
-    <>
-
-    </>
+    <div className="content">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/passwordreset" element={<PasswordReset />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/pages/auth/PasswordReset.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PasswordReset = () => {
+    return (
+        <h1>Password Reset page</h1>
+    )
+}
+
+export default PasswordReset;
