b7206c9eef1a5926f6138e687eb114d40ae2093e
Tristin <tmille2004@gmail.com>
2023-02-07 01:33:13 -0600

    adds simple account page with functional logout button for redux

--- src/pages/account/Account.js
+++ src/pages/account/Account.js
@@ -1,10 +1,32 @@
 import React from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { logout } from "../../userSlice"
 
 const Account = () => {
+  const authenticated = useSelector((state) => state.auth.authenticated)
+  const user = useSelector((state) => state.auth.currentUser)
+  const dispatch = useDispatch()
 
-  return (
-    <h1>Account page</h1>
-  )
+  if (authenticated) {
+
+    return (
+      <>
+        <h1>{user.email}</h1>
+        <Link to="/logout" onClick={() => dispatch(logout())}>
+          Logout
+        </Link>
+      </>
+    )
+
+  } else {
+
+    return (
+      <h1>Please login</h1>
+    )
+
+  }
+
 }
 
 export default Account;
