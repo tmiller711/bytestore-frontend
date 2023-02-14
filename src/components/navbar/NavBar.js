d61c184567732319a99210cfc884ce6e051abbec
Tristin <tmille2004@gmail.com>
2023-02-13 23:25:34 -0600

    replaces login button with account button after login

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,16 +1,20 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
 import "./navbar.css"
 
 const NavBar = () => {
+  const authenticated = useSelector((state) => state.auth.authenticated)
 
   return (
     <div className="navbar">
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Register</Link></li>
-        <li style={{float: "right"}}><Link to="/account">Account</Link></li>
+        {authenticated ? 
+          <li style={{float: "right"}}><Link to="/account">Account</Link></li> : 
+          <li style={{float: "right"}}><Link to="/login">Login</Link></li> 
+        }
+          
       </ul>
     </div>
   )
